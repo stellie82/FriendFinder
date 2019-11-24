4b7498acfc62b863ba0b6bffeaae635a3be23d18
stellie82 <kims82@gmail.com>
2019-11-23 19:39:23 -0800

    un-comment out all questions for survey

--- app/data/questions.js
+++ app/data/questions.js
@@ -1,14 +1,14 @@
 // Create an array to hold survey questions
 var questions = [
-    // {"question": "You prefer to be indoors reading a book than out at a social event."},
-    // {"question": "Your favorite colors tend to be vibrant and colorful."},
-    // {"question": "Cuddling makes you feel warm and fuzzy on the inside."},
-    // {"question": "Your dream residence is on a large plot of land where you can run free."},
-    // {"question": "A spontaneous, last minute trip sounds exciting to you."},
-    // {"question": "People would say that you smile often."},
-    // {"question": "Some might say you are easily startled."},
-    // {"question": "Meat is a staple in your diet."},
-    // {"question": "You are more of a leader than a follower."},
+    {"question": "You prefer to be indoors reading a book than out at a social event."},
+    {"question": "Your favorite colors tend to be vibrant and colorful."},
+    {"question": "Cuddling makes you feel warm and fuzzy on the inside."},
+    {"question": "Your dream residence is on a large plot of land where you can run free."},
+    {"question": "A spontaneous, last minute trip sounds exciting to you."},
+    {"question": "People would say that you smile often."},
+    {"question": "Some might say you are easily startled."},
+    {"question": "Meat is a staple in your diet."},
+    {"question": "You are more of a leader than a follower."},
     {"question": "You enjoy the sound of your own voice."}
 ]
 
